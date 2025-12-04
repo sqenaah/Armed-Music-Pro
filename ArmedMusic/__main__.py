15943ffc9eea9910ef72747c55ff8159dc53c139
ArmedMusic User <armedmusic@example.com>
2025-12-04 18:30:16 +0400

    Temporarily disable pytgcalls to allow bot startup without music functions

--- ArmedMusic/__main__.py
+++ ArmedMusic/__main__.py
@@ -2,7 +2,7 @@ import asyncio
 import importlib
 
 from pyrogram import idle
-from pytgcalls.exceptions import NoActiveGroupCall
+# from pytgcalls.exceptions import NoActiveGroupCall  # Temporarily disabled
 
 import config
 from ArmedMusic import LOGGER, app, userbot
@@ -37,17 +37,18 @@ async def init():
         importlib.import_module("ArmedMusic.plugins" + all_module)
     LOGGER("ArmedMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await Anony.start()
-    try:
-        await Anony.stream_call("'https://i.ibb.co/gMkk9qC3/image.jpg")
-    except NoActiveGroupCall:
-        LOGGER("ArmedMusic").error(
-            "Please turn on the videochat of your log group\\channel.\n\nStopping Bot..."
-        )
-        exit()
-    except:
-        pass
-    await Anony.decorators()
+    # Temporarily disable music functions due to pytgcalls dependency issues
+    # await Anony.start()
+    # try:
+    #     await Anony.stream_call("'https://i.ibb.co/gMkk9qC3/image.jpg")
+    # except NoActiveGroupCall:
+    #     LOGGER("ArmedMusic").error(
+    #         "Please turn on the videochat of your log group\\channel.\n\nStopping Bot..."
+    #     )
+    #     exit()
+    # except:
+    #     pass
+    # await Anony.decorators()  # Temporarily disabled
     LOGGER("ArmedMusic").info(
         "\x41\x72\x6d\x65\x64\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x41\x72\x6d\x65\x64\x44\x65\x76"
     )
